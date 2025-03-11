1ca1a5f9cf98a3c459b8c300d23d7ecc6ad4852a
egidio.pignataro <egidio.pignataro@gruppomol.it>
2025-03-12 00:49:13 +0100

    added assets manager

--- src/Interfaces/AssetsManagerInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DumpsterfireBase\Interfaces;
-
-interface AssetsManagerInterface
-{
-    public function loadJs(string $path): self;
-    public function loadCss(string $path): self;
-}
